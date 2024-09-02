d6bd25dc4c110a955d893be5f46b4d1fa0cbd7b6
hassamkkhansaleem <hassamkhan.zamstudios@gmail.com>
2024-09-02 16:52:09 +0500

    basic website

---
+++ header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header page</title>
+</head>
+
+<body>
+    <h1>BASIC WEBSITE IN PHP</h1>
+    <header>
+        <h3>
+            LOGO
+        </h3>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+        </header>
+</body>
+
+</html>
